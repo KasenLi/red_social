cf028e9965f5d81d59c4c23662ae0bddd31b5023
KasenLi <kasenkelvin.10@gmail.com>
2018-04-17 18:17:36 -0400

    Se relacionaron los modelos

--- app/Post.php
+++ app/Post.php
@@ -9,4 +9,11 @@ class Post extends Model
     protected $table = "posts";
 
     protected $fillable = ['body', 'user_id'];
+
+    public function user(){
+    	return $this->belongsTo('App\User');
+    }
+    public function comments(){
+    	return $this->hasMany('App\Comment');
+    }
 }
