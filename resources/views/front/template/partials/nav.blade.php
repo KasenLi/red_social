f1fcb6d3c725ba28a382db7828cd5ced3f5ff218
KasenLi <kasenkelvin.10@gmail.com>
2018-04-17 23:23:49 -0400

    Se configuro el logout

--- resources/views/front/template/partials/nav.blade.php
+++ resources/views/front/template/partials/nav.blade.php
@@ -26,7 +26,15 @@
           <a href="#" class="nav-link dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" id="navbarDropdown">{{ Auth::user()->name }} <span class="caret"></span></a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
             <a href="#" class="dropdown-item">Perfil</a>
-            <a href="{{ route('logout')}}" class="dropdown-item">Salir</a>
+            <a class="dropdown-item" href="{{ route('logout') }}"
+               onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+                {{ __('Salir') }}
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
           </div>
         </li>
       @endguest
