6346dfb3e10f6ae23880dd274e67bd1d81ee15f4
KasenLi <kasenkelvin.10@gmail.com>
2018-04-20 03:40:35 -0400

    belongsTo Post

--- app/PostLike.php
+++ app/PostLike.php
@@ -9,4 +9,8 @@ class PostLike extends Model
     protected $table = "post_likes";
 
     protected $fillable = ['user_id', 'post_id'];
+
+    public function post(){
+    	return $this->belongsTo('App\Post');
+    }
 }
